58ddec4aeaf90ae7102f96f6b9b4955a09f7263b
Lenovo <raintruwalker@gmail.com>
2023-05-14 22:56:53 +0300

    Update DLCPage page

--- src/pages/dlcs/[dlcId].jsx
+++ src/pages/dlcs/[dlcId].jsx
@@ -6,8 +6,11 @@ import {addGame} from "../../redux/actions/cart";
 import * as StyledGamePage from "../../components/StyledGamePage";
 import {convertCurrency} from "../../lib/Currency";
 import YouTubeVideo from "../../components/YouTubeVideo";
-import {Person} from "@mui/icons-material";
-import {formatDateString} from "../../lib/Game";
+import {Download, Person} from "@mui/icons-material";
+import {fileHandler, formatDateString} from "../../lib/Game";
+import {useForm} from "react-hook-form";
+import * as ModalForm from "../../components/ModalForm";
+import ModalWindow from "../../components/ModalWindow";
 
 const DLCPage = () => {
     const currency = useContext(CurrencyContext)
@@ -24,22 +27,27 @@ const DLCPage = () => {
         }
     ])
     const [newReview, setNewReview] = useState('')
+    const [modalActive, setModalActive] = useState(false)
+    const [currentWindow, setCurrentWindow] = useState(null)
+    const {register, handleSubmit} = useForm()
+    const [imageUrl, setImageUrl] = useState("/images/defaultGame.png")
+    const [file, setFile] = useState(null)
 
     const DLCs = [
         {
             id: "1-dlc-1",
             name: "DLC 1",
             price: 1499,
-            image: "/images/product_1.jpg",
+            image: "/images/dlc_1.jpg",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
             videoId: "Oj5e6oHolkA",
-            new: "изменение1\nизменение2\nизменение3"
+            new: "изменение1.изменение2.изменение3"
         },
         {
             id: "2-dlc-2",
             name: "DLC 2",
             price: 1999,
-            image: "/images/product_2.jpg",
+            image: "/images/dlc_2.png",
             description:
                 "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
             videoId: "56bh-ROgVlQ",
@@ -49,43 +57,7 @@ const DLCPage = () => {
             id: "3-dlc-3",
             name: "DLC 3",
             price: 2299,
-            image: "/images/product_3.jpg",
-            description: "Excepteur sint occaecat cupidatat non proident.",
-            videoId: "EtF6oSFRFWo",
-            new: "изменение1\nизменение2\nизменение3"
-        },
-        {
-            id: "4-dlc-4",
-            name: "DLC 4",
-            price: 1299,
-            image: "/images/product_4.jpg",
-            description: "Excepteur sint occaecat cupidatat non proident.",
-            videoId: "EtF6oSFRFWo",
-            new: "изменение1\nизменение2\nизменение3"
-        },
-        {
-            id: "5-dlc-5",
-            name: "DLC 5",
-            price: 3299,
-            image: "/images/product_5.jpg",
-            description: "Excepteur sint occaecat cupidatat non proident.",
-            videoId: "EtF6oSFRFWo",
-            new: "изменение1\nизменение2\nизменение3"
-        },
-        {
-            id: "6-dlc-6",
-            name: "DLC 6",
-            price: 599,
-            image: "/images/product_6.jpg",
-            description: "Excepteur sint occaecat cupidatat non proident.",
-            videoId: "EtF6oSFRFWo",
-            new: "изменение1\nизменение2\nизменение3"
-        },
-        {
-            id: "7-dlc-7",
-            name: "DLC 7",
-            price: 199,
-            image: "/images/product_7.jpg",
+            image: "/images/dlc_3.jpg",
             description: "Excepteur sint occaecat cupidatat non proident.",
             videoId: "EtF6oSFRFWo",
             new: "изменение1\nизменение2\nизменение3"
@@ -95,7 +67,8 @@ const DLCPage = () => {
     useEffect(() => {
         const selectedDlc = DLCs.find((game) => game.id === dlcId)
         setDlc(selectedDlc)
-    }, [dlcId])
+        fileHandler(file, setImageUrl)
+    }, [file, dlcId])
 
     if (!dlc) {
         return <div>Loading...</div>;
@@ -122,8 +95,21 @@ const DLCPage = () => {
         setNewReview('')
     }
 
+    const handleUpdate = (data) => {
+    }
+    const handleDelete = (data) => {
+    }
+
     return (
         <StyledGamePage.GameCard>
+            <StyledGamePage.UpdateIcon fontSize="large" color="warning" onClick={() => {
+                setCurrentWindow("update")
+                setModalActive(true)
+            }}/>
+            <StyledGamePage.DeleteIcon fontSize="large" color="error" onClick={() => {
+                setCurrentWindow("delete")
+                setModalActive(true)
+            }}/>
             <StyledGamePage.Game>
                 <StyledGamePage.GameImage src={dlc.image} alt={dlc.name}/>
                 <StyledGamePage.GameContent>
@@ -143,7 +129,7 @@ const DLCPage = () => {
             <StyledGamePage.ChangelogContainer>
                 <StyledGamePage.ChangelogTitle>Нововведения:</StyledGamePage.ChangelogTitle>
                 <StyledGamePage.ChangelogList>
-                    {dlc.new.split('\n').map((item, index) => (
+                    {dlc.new.split('.').map((item, index) => (
                         <StyledGamePage.ChangelogItem key={index}>{item}</StyledGamePage.ChangelogItem>
                     ))}
                 </StyledGamePage.ChangelogList>
@@ -170,6 +156,47 @@ const DLCPage = () => {
                     <StyledGamePage.GameButton type="submit">Добавить отзыв</StyledGamePage.GameButton>
                 </StyledGamePage.AddReviewForm>
             </StyledGamePage.ReviewsSection>
+            <ModalWindow active={modalActive} setActive={setModalActive}>
+                <ModalForm.Form onSubmit={handleSubmit(handleUpdate)}
+                                style={{display: currentWindow !== "update" && "none"}}>
+                    <ModalForm.Title>Изменить DLC</ModalForm.Title>
+                    <ModalForm.MainContainer>
+                        <ModalForm.LeftFormContainer>
+                            <ModalForm.FormImage
+                                src={imageUrl !== "/images/defaultGame.png" ? imageUrl : dlc.image}
+                                alt="Изображение игры"/>
+                            <ModalForm.FormLabel style={{cursor: "pointer"}} htmlFor="fileUpdate">
+                                <Download/>
+                            </ModalForm.FormLabel>
+                            <ModalForm.FormInput style={{opacity: 0, pointerEvents: "none"}} type="file"
+                                                 id="fileUpdate" {...register("fileUpdate")}
+                                                 onChange={(event) => {
+                                                     setFile(event.target.files[0])
+                                                     console.log(event.target.files[0])
+                                                 }} accept="image/*"/>
+                        </ModalForm.LeftFormContainer>
+                        <ModalForm.RightFormContainer>
+                            <ModalForm.FormInput required type="text" defaultValue={dlc.name} {...register("nameUpdate")}
+                                                 placeholder="Наименование DLC"/>
+                            <ModalForm.FormInput required type="text" defaultValue={dlc.description} {...register("descUpdate")}
+                                                 placeholder="Описание DLC"/>
+                            <ModalForm.FormInput required type="number" min="0"
+                                                 defaultValue={dlc.price} {...register("priceUpdate")} placeholder="Цена"/>
+                            <ModalForm.FormInput required type="text"
+                                                 defaultValue={dlc.videoId} {...register("youtubeUpdate")}
+                                                 placeholder="Id трейлера"/>
+                            <ModalForm.FormInput required type="text" defaultValue={dlc.new} {...register("newMUpdate")}
+                                                 placeholder="Нововведения"/>
+                            <ModalForm.FormButton>Изменить</ModalForm.FormButton>
+                        </ModalForm.RightFormContainer>
+                    </ModalForm.MainContainer>
+                </ModalForm.Form>
+                <ModalForm.Form onSubmit={handleSubmit(handleDelete)}
+                                style={{display: currentWindow !== "delete" && "none"}}>
+                    <ModalForm.Title>Удалить DLC</ModalForm.Title>
+                    <ModalForm.FormButton>Удалить</ModalForm.FormButton>
+                </ModalForm.Form>
+            </ModalWindow>
         </StyledGamePage.GameCard>
     )
 }
