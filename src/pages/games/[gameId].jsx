eec7697acc0aa276e7b683f439b790c39ee0742c
Lenovo <raintruwalker@gmail.com>
2023-05-14 22:57:10 +0300

    Update GamePage page

--- src/pages/games/[gameId].jsx
+++ src/pages/games/[gameId].jsx
@@ -6,8 +6,11 @@ import {addGame} from "../../redux/actions/cart";
 import {useDispatch} from "react-redux";
 import {useRouter} from "next/router";
 import YouTubeVideo from "../../components/YouTubeVideo";
-import {Person} from "@mui/icons-material";
-import {formatDateString} from "../../lib/Game";
+import {Download, Person} from "@mui/icons-material";
+import {fileHandler, formatDateString} from "../../lib/Game";
+import ModalWindow from "../../components/ModalWindow";
+import * as ModalForm from "../../components/ModalForm";
+import {useForm} from "react-hook-form";
 
 const GamePage = () => {
     const currency = useContext(CurrencyContext)
@@ -24,6 +27,11 @@ const GamePage = () => {
         }
     ])
     const [newReview, setNewReview] = useState('')
+    const [modalActive, setModalActive] = useState(false)
+    const [currentWindow, setCurrentWindow] = useState(null)
+    const {register, handleSubmit} = useForm()
+    const [imageUrl, setImageUrl] = useState("/images/defaultGame.png")
+    const [file, setFile] = useState(null)
 
     const games = [
         {
@@ -33,7 +41,7 @@ const GamePage = () => {
             image: "/images/product_1.jpg",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
             videoId: "Oj5e6oHolkA",
-            technicalRequirements: "Рекомендуемые требования:\nОперационная система: Windows 10\nПроцессор: Intel Core i7\nОперативная память: 16 ГБ\nВидеокарта: NVIDIA GeForce RTX 3080\nПоддержка DirectX 12"
+            technicalRequirements: "Рекомендуемые требования:.Операционная система: Windows 10.Процессор: Intel Core i7.Оперативная память: 16 ГБ.Видеокарта: NVIDIA GeForce RTX 3080.Поддержка DirectX 12"
         },
         {
             id: "2-game-2",
@@ -97,33 +105,37 @@ const GamePage = () => {
             id: "1-dlc-1",
             name: "DLC 1",
             price: 1499,
-            image: "/images/product_1.jpg",
+            image: "/images/dlc_1.jpg",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
             videoId: "Oj5e6oHolkA",
+            new: "изменение1\nизменение2\nизменение3"
         },
         {
             id: "2-dlc-2",
             name: "DLC 2",
             price: 1999,
-            image: "/images/product_2.jpg",
+            image: "/images/dlc_2.png",
             description:
                 "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
             videoId: "56bh-ROgVlQ",
+            new: "изменение1\nизменение2\nизменение3"
         },
         {
             id: "3-dlc-3",
             name: "DLC 3",
             price: 2299,
-            image: "/images/product_3.jpg",
+            image: "/images/dlc_3.jpg",
             description: "Excepteur sint occaecat cupidatat non proident.",
             videoId: "EtF6oSFRFWo",
+            new: "изменение1\nизменение2\nизменение3"
         }
     ]
 
     useEffect(() => {
         const selectedGame = games.find((game) => game.id === gameId)
         setGame(selectedGame)
-    }, [gameId])
+        fileHandler(file, setImageUrl)
+    }, [file, gameId])
 
     if (!game) {
         return <div>Loading...</div>;
@@ -150,8 +162,27 @@ const GamePage = () => {
         setNewReview('')
     }
 
+    const handleDLC = (data) => {
+    }
+    const handleUpdate = (data) => {
+    }
+    const handleDelete = (data) => {
+    }
+
     return (
         <StyledGamePage.GameCard>
+            <StyledGamePage.DLCIcon fontSize="large" color="success" onClick={() => {
+                setCurrentWindow("dlc")
+                setModalActive(true)
+            }}/>
+            <StyledGamePage.UpdateIcon fontSize="large" color="warning" onClick={() => {
+                setCurrentWindow("update")
+                setModalActive(true)
+            }}/>
+            <StyledGamePage.DeleteIcon fontSize="large" color="error" onClick={() => {
+                setCurrentWindow("delete")
+                setModalActive(true)
+            }}/>
             <StyledGamePage.Game>
                 <StyledGamePage.GameImage src={game.image} alt={game.name}/>
                 <StyledGamePage.GameContent>
@@ -171,7 +202,7 @@ const GamePage = () => {
             <StyledGamePage.GameDetailsContainer>
                 <StyledGamePage.TechnicalRequirementsContainer>
                     <StyledGamePage.TechnicalRequirementsList>
-                        {game.technicalRequirements.split('\n').map((item, index) => (
+                        {game.technicalRequirements.split('.').map((item, index) => (
                             <StyledGamePage.TechnicalRequirementsItem
                                 key={index}>{item}</StyledGamePage.TechnicalRequirementsItem>
                         ))}
@@ -212,6 +243,80 @@ const GamePage = () => {
                     <StyledGamePage.GameButton type="submit">Добавить отзыв</StyledGamePage.GameButton>
                 </StyledGamePage.AddReviewForm>
             </StyledGamePage.ReviewsSection>
+            <ModalWindow active={modalActive} setActive={setModalActive}>
+                <ModalForm.Form onSubmit={handleSubmit(handleUpdate)}
+                                style={{display: currentWindow !== "dlc" && "none"}}>
+                    <ModalForm.Title>Добавить DLC</ModalForm.Title>
+                    <ModalForm.MainContainer>
+                        <ModalForm.LeftFormContainer>
+                            <ModalForm.FormImage
+                                src={imageUrl !== "/images/defaultGame.png" ? imageUrl : "/images/defaultGame.png"}
+                                alt="Изображение DLC"/>
+                            <ModalForm.FormLabel style={{cursor: "pointer"}} htmlFor="fileUpdate">
+                                <Download/>
+                            </ModalForm.FormLabel>
+                            <ModalForm.FormInput style={{opacity: 0, pointerEvents: "none"}} type="file"
+                                                 id="fileUpdate" {...register("fileUpdate")}
+                                                 onChange={(event) => {
+                                                     setFile(event.target.files[0])
+                                                     console.log(event.target.files[0])
+                                                 }} accept="image/*"/>
+                        </ModalForm.LeftFormContainer>
+                        <ModalForm.RightFormContainer>
+                            <ModalForm.FormInput required type="text" {...register("nameAdd")}
+                                                 placeholder="Наименование DLC"/>
+                            <ModalForm.FormInput required type="text" {...register("descAdd")}
+                                                 placeholder="Описание DLC"/>
+                            <ModalForm.FormInput required type="number" min="0"{...register("priceAdd")}
+                                                 placeholder="Цена"/>
+                            <ModalForm.FormInput required type="text" {...register("youtubeAdd")}
+                                                 placeholder="Id трейлера"/>
+                            <ModalForm.FormInput required type="text" {...register("newMAdd")}
+                                                 placeholder="Нововведения"/>
+                            <ModalForm.FormButton>Добавить</ModalForm.FormButton>
+                        </ModalForm.RightFormContainer>
+                    </ModalForm.MainContainer>
+                </ModalForm.Form>
+                <ModalForm.Form onSubmit={handleSubmit(handleUpdate)}
+                                style={{display: currentWindow !== "update" && "none"}}>
+                    <ModalForm.Title>Изменить игру</ModalForm.Title>
+                    <ModalForm.MainContainer>
+                        <ModalForm.LeftFormContainer>
+                            <ModalForm.FormImage
+                                src={imageUrl !== "/images/defaultGame.png" ? imageUrl : game.image}
+                                alt="Изображение игры"/>
+                            <ModalForm.FormLabel style={{cursor: "pointer"}} htmlFor="fileUpdate">
+                                <Download/>
+                            </ModalForm.FormLabel>
+                            <ModalForm.FormInput style={{opacity: 0, pointerEvents: "none"}} type="file"
+                                                 id="fileUpdate" {...register("fileUpdate")}
+                                                 onChange={(event) => {
+                                                     setFile(event.target.files[0])
+                                                     console.log(event.target.files[0])
+                                                 }} accept="image/*"/>
+                        </ModalForm.LeftFormContainer>
+                        <ModalForm.RightFormContainer>
+                            <ModalForm.FormInput required type="text" defaultValue={game.name} {...register("nameUpdate")}
+                                                 placeholder="Наименование игры"/>
+                            <ModalForm.FormInput required type="text" defaultValue={game.description} {...register("descUpdate")}
+                                                 placeholder="Описание игры"/>
+                            <ModalForm.FormInput required type="number" min="0"
+                                                 defaultValue={game.price} {...register("priceUpdate")} placeholder="Цена"/>
+                            <ModalForm.FormInput required type="text"
+                                                 defaultValue={game.videoId} {...register("youtubeUpdate")}
+                                                 placeholder="Id трейлера"/>
+                            <ModalForm.FormInput required type="text" defaultValue={game.technicalRequirements} {...register("newMUpdate")}
+                                                 placeholder="Системные требования"/>
+                            <ModalForm.FormButton>Изменить</ModalForm.FormButton>
+                        </ModalForm.RightFormContainer>
+                    </ModalForm.MainContainer>
+                </ModalForm.Form>
+                <ModalForm.Form onSubmit={handleSubmit(handleDelete)}
+                                style={{display: currentWindow !== "delete" && "none"}}>
+                    <ModalForm.Title>Удалить игру</ModalForm.Title>
+                    <ModalForm.FormButton>Удалить</ModalForm.FormButton>
+                </ModalForm.Form>
+            </ModalWindow>
         </StyledGamePage.GameCard>
     )
 }
