4081c64b6c38fb680c604e009f64f7e012c8a00f
Lenovo <raintruwalker@gmail.com>
2023-05-14 22:56:35 +0300

    Update DLCPage page

--- src/pages/dlcs/index.jsx
+++ src/pages/dlcs/index.jsx
@@ -13,65 +13,29 @@ const DLCsPage = () => {
             id: "1-dlc-1",
             name: "DLC 1",
             price: 1499,
-            image: "/images/product_1.jpg",
+            image: "/images/dlc_1.jpg",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
             videoId: "Oj5e6oHolkA",
-            technicalRequirements: "Рекомендуемые требования:\nОперационная система: Windows 10\nПроцессор: Intel Core i7\nОперативная память: 16 ГБ\nВидеокарта: NVIDIA GeForce RTX 3080\nПоддержка DirectX 12"
+            new: "изменение1\nизменение2\nизменение3"
         },
         {
             id: "2-dlc-2",
             name: "DLC 2",
             price: 1999,
-            image: "/images/product_2.jpg",
+            image: "/images/dlc_2.png",
             description:
                 "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
             videoId: "56bh-ROgVlQ",
-            technicalRequirements: "Рекомендуемые требования:\nОперационная система: Windows 10\nПроцессор: Intel Core i7\nОперативная память: 16 ГБ\nВидеокарта: NVIDIA GeForce RTX 3080\nПоддержка DirectX 12"
+            new: "изменение1\nизменение2\nизменение3"
         },
         {
             id: "3-dlc-3",
             name: "DLC 3",
             price: 2299,
-            image: "/images/product_3.jpg",
+            image: "/images/dlc_3.jpg",
             description: "Excepteur sint occaecat cupidatat non proident.",
             videoId: "EtF6oSFRFWo",
-            technicalRequirements: "Рекомендуемые требования:\nОперационная система: Windows 10\nПроцессор: Intel Core i7\nОперативная память: 16 ГБ\nВидеокарта: NVIDIA GeForce RTX 3080\nПоддержка DirectX 12"
-        },
-        {
-            id: "4-dlc-4",
-            name: "DLC 4",
-            price: 1299,
-            image: "/images/product_4.jpg",
-            description: "Excepteur sint occaecat cupidatat non proident.",
-            videoId: "EtF6oSFRFWo",
-            technicalRequirements: "Рекомендуемые требования:\nОперационная система: Windows 10\nПроцессор: Intel Core i7\nОперативная память: 16 ГБ\nВидеокарта: NVIDIA GeForce RTX 3080\nПоддержка DirectX 12"
-        },
-        {
-            id: "5-dlc-5",
-            name: "DLC 5",
-            price: 3299,
-            image: "/images/product_5.jpg",
-            description: "Excepteur sint occaecat cupidatat non proident.",
-            videoId: "EtF6oSFRFWo",
-            technicalRequirements: "Рекомендуемые требования:\nОперационная система: Windows 10\nПроцессор: Intel Core i7\nОперативная память: 16 ГБ\nВидеокарта: NVIDIA GeForce RTX 3080\nПоддержка DirectX 12"
-        },
-        {
-            id: "6-dlc-6",
-            name: "DLC 6",
-            price: 599,
-            image: "/images/product_6.jpg",
-            description: "Excepteur sint occaecat cupidatat non proident.",
-            videoId: "EtF6oSFRFWo",
-            technicalRequirements: "Рекомендуемые требования:\nОперационная система: Windows 10\nПроцессор: Intel Core i7\nОперативная память: 16 ГБ\nВидеокарта: NVIDIA GeForce RTX 3080\nПоддержка DirectX 12"
-        },
-        {
-            id: "7-dlc-7",
-            name: "DLC 7",
-            price: 199,
-            image: "/images/product_7.jpg",
-            description: "Excepteur sint occaecat cupidatat non proident.",
-            videoId: "EtF6oSFRFWo",
-            technicalRequirements: "Рекомендуемые требования:\nОперационная система: Windows 10\nПроцессор: Intel Core i7\nОперативная память: 16 ГБ\nВидеокарта: NVIDIA GeForce RTX 3080\nПоддержка DirectX 12"
+            new: "изменение1\nизменение2\nизменение3"
         }
     ]
 
